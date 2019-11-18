005fdf4a105da8c17a242d1bf6c1c982fe463619
ToA <nansh9815@naver.com>
2019-11-18 21:20:42 +0900

    dfdf

--- kakao/login_form.php
+++ kakao/login_form.php
@@ -5,7 +5,7 @@ echo $user;
 <script type="text/javascript" src="./login.js"></script>
 
 <div id="login_form">
-    <form name="login" method="post" action="login.php">
+    <form name="login_form" method="post" action="login.php">
         <input type="text" name="pw" id="pw">
         <input type="hidden" name="user" value="<?=$user?>">
         <button onclick="check_input()">제출</button>
