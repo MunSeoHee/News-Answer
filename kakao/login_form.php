3178c27085d37f7543b1d6a2db09a2b798ab8c2d
ToA <nansh9815@naver.com>
2019-11-18 21:11:27 +0900

    dfdf

---
+++ kakao/login_form.php
@@ -0,0 +1,22 @@
+<?php
+$user = $_GET["user"];
+echo $user;
+?>
+
+<form name="login" method="post" action="input.php">
+    <input type="text" name="pw">
+    <input type="hidden" name="user" value="<?=$user?>">
+    <button onclick="check_input()"></button>
+</form>
+
+<script>
+function check_input(){
+    if(!document.login_form.pw.value){
+        aletr("비밀번호를 입력하세요");
+        document.login_form.pw.focus();
+        return;
+    }
+    document.login_form.submit();
+}
+
+</script>
