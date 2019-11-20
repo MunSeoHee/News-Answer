7d2a53f6953f37d25e0be72ce19b7fc85d9a5602
ToA <nansh9815@naver.com>
2019-11-20 15:49:53 +0900

    dsf

--- kakao/message.php
+++ kakao/message.php
@@ -222,7 +222,7 @@ EOD;
             $result = mysqli_query($con, $sql);
             $curriculum = '';
             while( $row = mysqli_fetch_array($result) ) {
-                $curriculum = $curriculum.$row['year']."학년 ".$row['semester'].$row['subject']." ".$row['point']." 학점 ".$row['division'].'\n';
+                $curriculum = $curriculum.$row['year']."학년 ".$row['semester']." [".$row['division'].'] '.$row['subject']." ".$row['point']." 학점 ".'\n';
               }
             echo <<<EOD
                 {
