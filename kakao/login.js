dacf2d914023fe8c8f36f791f286eb204ff36dee
ToA <nansh9815@naver.com>
2019-12-03 14:24:18 +0900

    dfsf

--- kakao/login.js
+++
@@ -1,9 +0,0 @@
-function check_input(){
-    if(!document.login_form.pw.value){
-        aletr("비밀번호를 입력하세요");
-        document.login_form.pw.focus();
-        return;
-    }
-    document.login_form.submit();
-}
-
