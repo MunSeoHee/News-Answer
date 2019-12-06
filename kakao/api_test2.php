a01df79cd3bcd6b5f48086e31328b75318d3817d
ToA <nansh9815@naver.com>
2019-12-06 15:31:21 +0900

    dsfsdf

--- kakao/api_test2.php
+++ kakao/api_test2.php
@@ -35,7 +35,7 @@ $body = substr($res, $header_size);
 $body_json = json_decode($body, true);
 print_r($body_json);
 
-echo '<br>'.$body_json["labels"]["sentenceIndices"];
+echo '<br>'.$body_json["labels"]["sentenceIndices"][0];
 
 curl_close($ch);
 
