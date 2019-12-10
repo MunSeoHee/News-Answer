89edd44529944ef3a462a18bd732586efec491ab
yeon9 <husn0504@naver.com>
2019-12-10 16:19:59 +0900

    dddd

--- kakao/sh_test.php
+++ kakao/sh_test.php
@@ -27,8 +27,16 @@ foreach($result as $url){
     //explode('',$변수) -> ''을 기준으로 $변수 의 내용을 자르게 됨. ''기준으로 앞이 0번째 뒤가 1번째
     $text = explode('articleBodyContents', $content);
     $text = explode('</script>', $text[1]);
-    $text = explode('▶', $text[1]);
-    $text = $text[0];
+    if(strrpos($text, '<a href="http://naver.me/GxmvUNz3" target="_blank">'){
+        $text = explode('<a href="http://naver.me/GxmvUNz3" target="_blank"', $text[1]);
+        $text = $text[0];
+        echo "국민일보";
+    }
+    else{
+        $text = explode('▶', $text[1]);
+         $text = $text[0];
+    }
+    
     
     $script = preg_replace("(\<(/?[^\>]+)\>)", "", $text);
     $script = trim($script);
