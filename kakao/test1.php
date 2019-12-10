dab48e835fc4ccce0f610e13a1534490f49defa8
yeon9 <husn0504@naver.com>
2019-12-10 14:21:41 +0900

    dddd

--- kakao/test1.php
+++ kakao/test1.php
@@ -32,15 +32,15 @@ $sql = "select * from news where url ='$plan[0]'";
 $result = mysqli_query($con, $sql);
 $result = mysqli_num_rows($result);
 
-echo $today;
-/*
+
 if ($result){
 }
 else{
-    $sql = "insert into news (url, date, categorie) values ('$plan[0]', '$today', 1)";
+    $sql = "insert into news (url, date, categorie) values ('$plan[0]', $today, 1)";
     mysqli_query($con, $sql);
+    echo "ok";
 }
-*/
+
 //$rank_p = [];
 //$rank_p[0] = $plan[0];
 
