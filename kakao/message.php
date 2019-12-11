40ce9b06971d48ab754544c374c072fbc9286724
ToA <nansh9815@naver.com>
2019-12-11 14:19:48 +0900

    sfsdf

--- kakao/message.php
+++ kakao/message.php
@@ -33,7 +33,7 @@
             {
                 "message":
                 {
-                    "text": "$summ"
+                    "text": '$summ'
                 },
                 "keyboard":
                 {
