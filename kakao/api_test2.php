f870dd62b7c6345e02d9f19de83082baeb0aa6fe
ToA <nansh9815@naver.com>
2019-12-11 14:51:48 +0900

    dsfd

--- kakao/api_test2.php
+++ kakao/api_test2.php
@@ -4,15 +4,15 @@
 include_once('./setting.php');
 
 $url = 'https://api.maum.ai/api/bert.xdc/'; //접속할 url 입력
-$sql = "select script from news where number=121";
+$sql = "select script from news where number=$number";
 $result = mysqli_query($con, $sql);
 
 foreach($result as $res){
     $text = $res['script'];
-    echo $text;
+    // echo $text;
 }
-// $sql = "update user set news=$number where user_key='$userkey'";
-// mysqli_query($con, $sql);
+$sql = "update user set news=$number where user_key='$userkey'";
+mysqli_query($con, $sql);
 
 $summ='';
 
@@ -56,7 +56,7 @@ for($i=0; $i<2; $i++){
     $end = (int)$end;
 
     //문장
-    echo '<pre>'.iconv_substr($text, $start, $end - $start, "utf-8").'<pre>';
+    // echo '<pre>'.iconv_substr($text, $start, $end - $start, "utf-8").'<pre>';
     $summ = $summ.iconv_substr($text, $start, $end - $start, "utf-8");
 }
 $summ = str_replace('"',"'",$summ);
