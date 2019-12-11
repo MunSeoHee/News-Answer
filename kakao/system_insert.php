05dcc4aa8408906bca993a8569c306a87928d752
ToA <nansh9815@naver.com>
2019-12-11 15:59:37 +0900

    df

--- kakao/system_insert.php
+++ kakao/system_insert.php
@@ -1,6 +1,6 @@
 <?php
 include_once('./setting.php');
-
-$sql = "insert into system (user, date, url, request, response) values ($userkey, now(), $url, $request, $response)";
+$today = date("Y-m-d H:i:s");
+$sql = "insert into system (user, date, url, request, response) values ('$userkey', '$today', '$url', '$request', '$response')";
 mysqli_query($con, $sql);
 ?>
