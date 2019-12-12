49eeea0dea4e14dc458dcf7bbf165503b5cc188d
DY Lee <ldy1331@gmail.com>
2019-12-12 14:03:58 +0900

    dddd

--- kakao/test3.php
+++ kakao/test3.php
@@ -6,15 +6,15 @@ $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL, 'https://news.naver.com/');
 // 헤더는 제외하고 content 만 받음
-curl_setopt($ch, CURLOPT_HEADER, 0); 
+curl_setopt($ch, CURLOPT_HEADER, 0);
 // 응답 값을 브라우저에 표시하지 말고 값을 리턴
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 // 브라우저처럼 보이기 위해 user agent 사용
 curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.5) Gecko/20041107 Firefox/1.0'); 
 //크롤링해온 내용 content 변수에 저장
-$content = curl_exec($ch); 
+$content = curl_exec($ch);
 //인코딩이 utf-8이 아닌경우에만 사용
-$content = iconv('euc-kr','utf-8',$content);
+$content = iconv('euc-kr','utf-8', $content);
 
 $now = date("Y-m-d H:i:s");
 
