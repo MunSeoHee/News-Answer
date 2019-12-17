683e358284b9174e0c17a5fb6953da6710e9613e
DY Lee <ldy1331@gmail.com>
2019-12-17 14:59:34 +0900

    dddd

--- kakao/admin.php
+++ kakao/admin.php
@@ -1,4 +1,4 @@
-<html style="width: 100%;">
+<html style="width: screen.width !important;">
 <head>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 <script language='javascript' src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
