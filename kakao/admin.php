f29eb68c995a420e700b31c67edf452002add627
DY Lee <ldy1331@gmail.com>
2019-12-17 14:52:52 +0900

    dddd

--- kakao/admin.php
+++ kakao/admin.php
@@ -1,11 +1,11 @@
-<html>
+<html style="width: 100%;">
 <head>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 <script language='javascript' src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script language='javascript' src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script language='javascript' src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </head>
-<body class="w-100">
+<body>
 <?php
 
 include_once('./setting.php');
