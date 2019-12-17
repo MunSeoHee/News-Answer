15d2292dc10aa7b341501436e5a59b621d8c6fd7
DY Lee <ldy1331@gmail.com>
2019-12-17 14:38:54 +0900

    dddd

--- kakao/admin.php
+++ kakao/admin.php
@@ -2,6 +2,11 @@
 <script language='javascript' src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script language='javascript' src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script language='javascript' src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<style>
+  body {
+    width: screen.width !important;
+  }
+</style>
 
 <?php
 
